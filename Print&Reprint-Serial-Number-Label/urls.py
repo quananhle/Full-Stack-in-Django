0cf226350269f16ac4178d823947e2a9a03a6454
Quan Le <35042430+Quananhle@users.noreply.github.com>
2022-03-02 11:46:51 -0600

    Create urls.py

---
+++ Print&Reprint-Serial-Number-Label/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'mfg'
+
+# =================== WIP URLS ============================
+urlpatterns += [
+    path('wip/mo_manager/detail/<pk>', views.WipModelManagerDetailView.as_view(), name='wip_mo_manager_detail'),
+    path('wip/mo_manager/update', views.mfg_material_master_update_serialization, name='mfg_serialization_update'),
+    path('wip/mo_manager/update/serialization', views.mfg_material_update_serialization, name='mfg_material_update_serialization'),
+]
