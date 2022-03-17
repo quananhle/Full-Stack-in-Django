2f47903b26394aed0b0d4152b5b48d58d7b59675
Quan Le <35042430+Quananhle@users.noreply.github.com>
2022-03-17 10:39:56 -0500

    Update mfg_auditsortedconfig.sql

--- Database/Table/mfg_auditsortedconfig.sql
+++ Database/Table/mfg_auditsortedconfig.sql
@@ -1,6 +1,3 @@
-### mfg_auditsortedconfig
-![Audit Sorted Config](https://user-images.githubusercontent.com/35042430/158834018-1abab3e1-e13a-4fc7-bb9e-bc22dc56b8e0.png)
-
 -- Drop table
 
 -- DROP TABLE public.mfg_auditsortedconfig;
