6358bba7aee0408849c4a057f4ea260d7d134278
Quan Le <35042430+Quananhle@users.noreply.github.com>
2022-03-28 21:50:20 -0500

    Update urls.py

--- Component-Details-Update/Backend/urls.py
+++ Component-Details-Update/Backend/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'mfg'
 
 # =================== WIP URLS ============================
-urlpatterns += [
+urlpatterns = [
     path('wip/mo_manager/detail/<pk>', views.WipModelManagerDetailView.as_view(), name='wip_mo_manager_detail'),
     path('wip/mo_manager/update', views.mfg_material_master_update_serialization, name='mfg_serialization_update'),
     path('wip/mo_manager/update/serialization', views.mfg_material_update_serialization, name='mfg_material_update_serialization'),
