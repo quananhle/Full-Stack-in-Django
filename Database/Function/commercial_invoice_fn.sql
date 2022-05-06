21bbb26d7c9091f2ec645727e7bf6b2b969f3d5b
Quan Le <35042430+Quananhle@users.noreply.github.com>
2022-05-06 11:54:17 -0500

    Update commercial_invoice_fn.sql

--- Database/Function/commercial_invoice_fn.sql
+++ Database/Function/commercial_invoice_fn.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.msft_commercial_invoice_fn(transtype character varying, v_shiporderno character varying)
+CREATE OR REPLACE FUNCTION public.shp_commercial_invoice_fn(transtype character varying, v_shiporderno character varying)
  RETURNS TABLE(rowid integer, field character varying, fieldname character varying, fieldvalue character varying)
  LANGUAGE plpgsql
 AS $function$
